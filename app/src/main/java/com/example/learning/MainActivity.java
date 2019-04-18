42fa2fc2f141086a43390927af38ba836d27ada3
DanilDubowitsky <48087527+DanilDubowitsky@users.noreply.github.com>
2019-04-18 14:40:35 +0700

    4

--- app/src/main/java/com/example/learning/MainActivity.java
+++ app/src/main/java/com/example/learning/MainActivity.java
@@ -2,16 +2,32 @@ package com.example.learning;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     Button button1;
+    Button button2;
+
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        button1 = (Button)findViewById(R.id.bt1);
-        
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button1 = (Button)findViewById(R.id.bt1);
+        button2 = (Button)findViewById(R.id.bt2);
+        textView = (TextView)findViewById(R.id.text);
+        button2.setOnClickListener(this);
+
     }
 
+    @Override
+    public void onClick(View v) {
+        if(v == button2){
+            nw k = new nw();
+            k.Settxt(textView);
+        }
+    }
 }
---
+++ app/src/main/java/com/example/learning/nw.java
@@ -0,0 +1,13 @@
+package com.example.learning;
+
+import android.widget.TextView;
+
+public class nw {
+    public nw() {
+
+    }
+    public void Settxt(TextView textView){
+        textView.setText("КОСТЯН ЛУЧШИЙ(НЕТ)");
+    }
+
+}
