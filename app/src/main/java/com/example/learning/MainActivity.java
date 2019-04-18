5ba9ab0992ebcfd3e86903f74e4661edf6ebad16
DanilDubowitsky <48087527+DanilDubowitsky@users.noreply.github.com>
2019-04-18 14:15:00 +0700

    2

--- app/src/main/java/com/example/learning/MainActivity.java
+++ app/src/main/java/com/example/learning/MainActivity.java
@@ -2,12 +2,16 @@ package com.example.learning;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button button1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        button1 = (Button)findViewById(R.id.bt1);
+        
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
 }
